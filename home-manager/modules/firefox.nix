155fd3e0a8471319c1d05204862f9f3aa2ab1f65
danninx <public.danninx@gmail.com>
2024-12-26 19:10:21 -0600

    Vimium firefox

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -17,6 +17,11 @@
                    install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
                    installation_mode = "force_installed";
                 };
+                #Vimium (vim keybinds for )
+                "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+                  install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
+                  installation_mode = "normal_installed";
+                };
             };
             OverrideFirstRunPage = "";
             OverridePostUpdatePage = "";
