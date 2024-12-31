a8b86567b229431b4ee3b6826ed664e23c938769
danninx <public.danninx@gmail.com>
2024-12-30 19:31:08 -0600

    Starting the great unification

--- flake.nix
+++ flake.nix
@@ -1,15 +1,18 @@
 {
-  description = "A very basic flake";
+  description = "Danninx configuration files";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nixvim = {
       url = "github:nix-community/nixvim";
     };
+    
     stylix = {
       url = "github:danth/stylix/release-24.11";
       inputs = {
@@ -19,10 +22,50 @@
     };
   };
 
-  outputs = { self, home-manager, nixpkgs, nixvim, stylix, ... } @ inputs:
+  outputs = { 
+    self, 
+    home-manager, 
+    nixpkgs, 
+    nixvim, 
+    stylix, 
+    ... 
+  } @ inputs:
+
   let
+
     inherit (self) outputs;
+
+    mkSystem = name: {
+      system,
+      extraModules ? [],
+    }: 
+
+    let
+      systemconfig = ./hosts/${name}/default.nix;
+      hardwareconfig = ./hosts/${name}/hardware.nix;
+      homeconfig = ./hosts/${name}/home.nix;
+    in
+      
+    with nixpkgs.lib; nixosSystem {
+      specialArgs = { inherit inputs outputs; };
+      system = "x86_64-linux";
+      modules = [
+        systemconfig
+        hardwareconfig
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.backupFileExtension = "hm-backup";
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.danninx = import homeconfig;
+          home-manager.extraSpecialArgs = { inherit inputs outputs system; };
+        }
+      ] ++ extraModules;
+    };
+
   in
+
   {
     nixosConfigurations = {
       "dtop" = nixpkgs.lib.nixosSystem {
