1562873b8a87e35be762db344bb7d72704965521
danninx <public.danninx@gmail.com>
2025-01-01 23:22:54 -0600

    How do I docker

--- nixos/modules/docker.nix
+++ nixos/modules/docker.nix
@@ -3,4 +3,15 @@
 {
   virtualisation.docker.enable = true;
   users.extraGroups.docker.members = [ "danninx" ];
+
+  # Testing container
+  virtualisation.oci-containers.containers.testing = {
+    image = "nixos/nix";
+    ports = [ "127.0.0.1:22:22" ];
+    hostname = "testing";
+    login = {
+      username = "danni";
+      passwordFile = builtins.toString ~/Documents/testing-password.txt;
+    };
+  };
 }
