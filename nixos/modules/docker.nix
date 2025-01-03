ef4e0e3c16cc4a3bddeb3affbbfc2b5706968568
danninx <public.danninx@gmail.com>
2025-01-02 20:30:49 -0600

    Still figuring out virtualisation :/

--- nixos/modules/docker.nix
+++ nixos/modules/docker.nix
@@ -8,17 +8,6 @@
     docker.enable = true;
     oci-containers = {
       backend = "docker";
-
-      containers.testing = {
-        image = "nixos/nix";
-        ports = [ "127.0.0.1:22:22" ];
-        hostname = "testing";
-        login = {
-          username = "danni";
-          passwordFile = "${config.users.users.danninx.home}/Documents/testing-password.txt";
-        };
-        autoStart = true;
-      };
     };
   };
 }
