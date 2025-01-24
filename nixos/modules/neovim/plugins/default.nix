697e88f568dd47b54e2585517ab0ab2380b5574d
danninx <public.danninx@gmail.com>
2025-01-24 12:44:59 -0600

    Neovim directory organization

--- nixos/modules/neovim/plugins/default.nix
+++ nixos/modules/neovim/plugins/default.nix
@@ -1,17 +1,17 @@
 {
   imports = [
-    ./bufferline.nix
-    ./cmp.nix
-    ./lualine.nix
-    # ./neo-tree.nix -- didn't like the side bar
-    ./mini.nix
-    ./oil.nix
-    ./telescope.nix
-    ./treesitter.nix
+    ./lang/cmp.nix
+    ./lang/treesitter.nix
+
+    ./nav/oil.nix
+    ./nav/telescope.nix
+
+    ./visual/bufferline.nix
+    ./visual/lualine.nix
+    ./visual/mini.nix
   ];
 
   programs.nixvim.plugins.nix.enable = true;
   programs.nixvim.plugins.nvim-autopairs.enable = true;
   programs.nixvim.plugins.lz-n.enable = true;
-  # programs.nixvim.plugins.web-devicons.enable = true;
 }
