c2464cd3ae77ac3f852785aecca024c89f589957
danninx <public.danninx@gmail.com>
2025-01-26 11:35:11 -0600

    Autologin fix

--- nixos/modules/display-manager.nix
+++ nixos/modules/display-manager.nix
@@ -4,14 +4,10 @@
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-<<<<<<< Updated upstream
     autoNumlock = true;
-    autoLogin.relogin = true;
-=======
     autoLogin = {
       enable = true;
       user = "danninx";
     };
->>>>>>> Stashed changes
   };
 }
