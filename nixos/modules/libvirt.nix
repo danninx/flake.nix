ccbc0f57259e890860806774484a4ad2e9369aba
danninx <public.danninx@gmail.com>
2025-05-08 19:08:40 -0500

    libvirt stuff
    Needed to passthrough my gpu for a project in windows (please for the love of god i need to remember to setup dual boot) vm, so I temporarily enabled vfio until the project was done

--- nixos/modules/libvirt.nix
+++ nixos/modules/libvirt.nix
@@ -1,28 +1,85 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-{
-  options = {
-    dnix.vms.enable = mkEnableOption "installation of libvirtd, virt-manager and associated packages";
-  };
+let
+  pciIDs = [
+    "10de:28a0"
+    "10de:22be"
+  ];
 
-  config = mkIf config.dnix.vms.enable {
-    programs.virt-manager.enable = true;
-    programs.dconf.enable = true;
+  gpuDriverId = "0000:01:00.0";
+in
+  with lib;
+  {
+    options = {
+      dnix.vms.enable = mkEnableOption "installation of libvirtd, virt-manager and associated packages";
+    };
+
+    config = mkIf config.dnix.vms.enable {
+      #boot.kernelModules = [
+      #  "vfio-pci"
+      #  "vfio_iommu_type1"
+      #  "vfio"
+      #];
+
+      #boot.kernelParams = [
+      #  "intel_iommu=on"
+      #  "iommu=pt"
+      #  ("vfio-pci.ids=" + builtins.concatStringsSep "," pciIDs)
+      #];
+
+      programs.virt-manager.enable = true;
+      programs.dconf.enable = true;
     # Note: make sure to run "virsh net-autostart default" for proper networking
 
-    users.groups.libvirtd.members = [ "danninx" ];
-    users.extraGroups.libvirtd.members = [ "danninx" ];
+      users.groups.libvirtd.members = [ "danninx" ];
+      users.extraGroups.libvirtd.members = [ "danninx" ];
 
-    virtualisation.libvirtd.enable = true;
-    virtualisation.spiceUSBRedirection.enable = true;
+      virtualisation = {
+        libvirtd = {
+          enable = true;
 
-    environment.systemPackages = with pkgs; [
-      virt-manager
-      virt-viewer
-      spice 
-      spice-gtk
-      spice-protocol
-    ];
+          qemu = {
+            package = pkgs.qemu_kvm;
+            ovmf = {
+              enable = true;
+              packages = [ (pkgs.OVMFFull.override {
+                secureBoot = true;
+                tpmSupport = true;
+              }).fd ];
+            };
+            swtpm.enable = true;
+          };
+        };
+
+        spiceUSBRedirection.enable = true;
+      };
+
+      services.spice-vdagentd.enable = true;
+
+      environment.systemPackages = with pkgs; [
+        virtio-win
+        virt-manager
+        virt-viewer
+        spice 
+        spice-gtk
+        spice-protocol
+        win-spice
+      ];
+
+      # force GPU to vfio
+      # systemd.services.forceRebindNVUSB = {
+      #   enable = true;
+      #   description = "forceRebindNvUsb";
+      #   wantedBy = [ "multi-user.target" ];
+      #   script = ''
+      #     echo -n "${gpuDriverId}" > /sys/bus/pci/drivers/xhci_hcd/unbind
+      #     echo -n "${gpuDriverId}" > /sys/bus/pci/driver/vfio-pci/bind
+      #   '';
+      #   serviceConfig = {
+      #     Type = "oneshot";
+      #     RemainsAfterExit = true;
+      #   };
+      # };
   };
+
 }
