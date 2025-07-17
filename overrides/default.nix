abf98faf22cc7e3a3ed42ce0ba70eaf9db3a4e18
danninx <public.danninx@gmail.com>
2025-07-17 01:36:00 -0500

    fix vscode override

--- overrides/default.nix
+++ overrides/default.nix
@@ -1,20 +1,11 @@
 self: super: {
   # not working
+  # discord = (super.discord.override {
+  #   commandLineArgs = "--disable-gpu --disable-software-rasterization";
+  # });
 
-  discord = super.discord.overrideAttrs (old: {
-    pname = "discord-wrapped";
-
-    nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ super.makeWrapper ];
-
-    postInstall = ''
-      mv $out/bin/discord $out/bin/discord-original
-      makeWrapper $out/bin/discord-original $out/bin/discord \
-      --add-flags "--enable-gpu-rasterization" \
-    '';
-  });
-
-  # not working
-  vscode.fhs = super.vscode.fhs.overrideAttrs (old: {
-    commandLineArgs = "--user-angle=vulkan";
-  });
+  vscode.fhs = (super.vscode.override {
+    commandLineArgs = "--use-angle=vulkan";
+  }).fhs;
 }
+  
